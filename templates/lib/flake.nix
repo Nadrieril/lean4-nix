cfb9ccf873108e7f6c98eb2fb948229cbda61ab7
Leni Aniva <v@leni.sh>
2024-10-14 21:21:24 -0700

    chore: Lock input version

--- templates/lib/flake.nix
+++ templates/lib/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    lean4-nix.url = "github:lenianiva/lean4-nix";
+    lean4-nix.url = "github:lenianiva/lean4-nix/v4.12.0";
   };
 
   outputs = inputs @ {
